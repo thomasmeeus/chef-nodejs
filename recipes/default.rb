fdf8a8771493bfa78af0ccc1ba77603c5ed088c4
Roderik van der Veer <roderik@vanderveer.be>
2014-01-08 16:42:35 +0100

    Feature: support OSX

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,15 +19,20 @@
 # limitations under the License.
 #
 
-include_recipe "apt"
+case node["platform_family"]
+    when 'mac_os_x'
+        include_recipe "homebrew"
 
-apt_repository "nodejs" do
-  uri "http://ppa.launchpad.net/chris-lea/node.js/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "C7917B12"
+    when 'debian'
+        include_recipe "apt"
+
+        apt_repository "nodejs" do
+          uri "http://ppa.launchpad.net/chris-lea/node.js/ubuntu"
+          distribution node['lsb']['codename']
+          components ["main"]
+          keyserver "keyserver.ubuntu.com"
+          key "C7917B12"
+        end
 end
 
 package "nodejs"
-
